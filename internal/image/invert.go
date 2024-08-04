534a092cd5b7e302f5ffe7ba516c628ebaa49d86
elbachir-one <bachiralfa@gmail.com>
2024-08-04 13:12:27 +0100

    consistent naming (Invrt >> invrt) in process method and also some typos

--- internal/image/invert.go
+++ internal/image/invert.go
@@ -6,45 +6,38 @@ import (
 	"image/color"
 )
 
+type Inverter struct{}
 
-type Inverter struct{
-
-}
-
-func (Invrt *Inverter) Process( img image.Image, theme string) (image.Image , error){
-
-	newImg , err := invertImage(img)
-
-	if err != nil{
-		return nil,err
+func (invrt *Inverter) Process(img image.Image, theme string) (image.Image, error) {
+	newImg, err := invertImage(img)
+	if err != nil {
+		return nil, err
 	}
-
-	return newImg,nil
+	return newImg, nil
 }
 
-func invertImage(img image.Image ) (image.Image, error){
+func invertImage(img image.Image) (image.Image, error) {
 	bounds := img.Bounds()
 	newImg := image.NewRGBA(bounds)
 
+	// Replace each pixel with the inverted ones
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			originalColor := img.At(x, y)
+			newColor := invertColor(originalColor)
+			newImg.Set(x, y, newColor)
+		}
+	}
 
-	// replace each pixel with the inverted ones
-    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-        for x := bounds.Min.X; x < bounds.Max.X; x++ {
-            originalColor := img.At(x, y)
-            newColor := invertColor(originalColor)
-            newImg.Set(x, y, newColor)
-        }
-    }
-	
-	if newImg == nil {return nil, errors.New("error processing the Image")} 
-
+	if newImg == nil {
+		return nil, errors.New("error processing the image")
+	}
 
 	return newImg, nil
-
 }
 
-// You can invert a color 
-func invertColor(clr color.Color) color.Color{
+// Invert a color
+func invertColor(clr color.Color) color.Color {
 	r, g, b, a := clr.RGBA()
 
 	return color.RGBA{
@@ -53,5 +46,4 @@ func invertColor(clr color.Color) color.Color{
 		B: uint8(255 - b/257),
 		A: uint8(a / 257),
 	}
-
 }

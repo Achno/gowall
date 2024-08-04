b5b9aec5a87bd1e79bc5c1e9017058975effe291
elbachir-one <bachiralfa@gmail.com>
2024-08-04 13:04:29 +0100

    Fixed formatting and spacing issues and some error handling

--- internal/image/image.go
+++ internal/image/image.go
@@ -18,146 +18,122 @@ import (
 
 // Available formats to Encode an image in
 var encoders = map[string]func(file *os.File, img image.Image) error{
-    "png": func(file *os.File, img image.Image) error {
-        return png.Encode(file, img)
-    },
-    "jpg": func(file *os.File, img image.Image) error {
-        return jpeg.Encode(file, img, nil)
-    },
-    "jpeg": func(file *os.File, img image.Image) error {
-        return jpeg.Encode(file, img, nil)
-    },
-    "webp": func(file *os.File, img image.Image) error {
-        return webp.Encode(file, img, nil)
-    },
+	"png": func(file *os.File, img image.Image) error {
+		return png.Encode(file, img)
+	},
+	"jpg": func(file *os.File, img image.Image) error {
+		return jpeg.Encode(file, img, nil)
+	},
+	"jpeg": func(file *os.File, img image.Image) error {
+		return jpeg.Encode(file, img, nil)
+	},
+	"webp": func(file *os.File, img image.Image) error {
+		return webp.Encode(file, img, nil)
+	},
 }
 
 // Create a Processor of this interface and call 'ProcessImg'
-type  ImageProcessor interface{
+type ImageProcessor interface {
 	Process(image.Image, string) (image.Image, error)
 }
 
-func LoadImage(filePath string) (image.Image , error){
-	
-	file,err := os.Open(filePath)
-
+func LoadImage(filePath string) (image.Image, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-
 	defer file.Close()
 
-	img,_,err := image.Decode(file)
-
-	return img,err
+	img, _, err := image.Decode(file)
+	return img, err
 }
 
-func SaveImage(img image.Image, filePath string, format string) error{
-
-	file,err := os.Create(filePath)
-
+func SaveImage(img image.Image, filePath string, format string) error {
+	file, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
-
 	defer file.Close()
 
-	
-    encoder, ok := encoders[strings.ToLower(format)]
+	encoder, ok := encoders[strings.ToLower(format)]
+	if !ok {
+		return fmt.Errorf("unsupported format: %s", format)
+	}
 
-    if !ok {
-        return fmt.Errorf("unsupported format: %s", format)
-    }
-
-    return encoder(file, img)
-
+	return encoder(file, img)
 }
 
-// 1. Loads the img, 2. Processes it depending on the type of Processor you put which impliments 
-// the 'ImageProcessor' interface, 3. Creates the necessary directories ,4. Saves the image there 
-func ProcessImg(imgPath string, processor ImageProcessor,theme string ) error {
-
+// 1. Loads the img, 2. Processes it depending on the type of Processor you put which implements
+// the 'ImageProcessor' interface, 3. Creates the necessary directories, 4. Saves the image there
+func ProcessImg(imgPath string, processor ImageProcessor, theme string) error {
 	img, err := LoadImage(imgPath)
-
 	if err != nil {
-		fmt.Println("Error loading image :", err)
+		fmt.Println("Error loading image:", err)
 		return err
 	}
 
-	newImg, err := processor.Process(img,theme)
-
+	newImg, err := processor.Process(img, theme)
 	if err != nil {
-		fmt.Println("Error processing image :", err)
+		fmt.Println("Error processing image:", err)
 		return err
 	}
 
-	//Extract file extension from imgPath
+	// Extract file extension from imgPath
 	extension := strings.ToLower(filepath.Ext(imgPath))
-
 	if extension == "" {
 		fmt.Println("Error: Could not determine file extension.")
-		return err
+		return fmt.Errorf("could not determine file extension")
 	}
 
-	// remove '.' from the extension
+	// Remove '.' from the extension
 	extension = extension[1:]
 
 	dirPath, err := utils.CreateDirectory()
-	nameOfFile := filepath.Base(imgPath)
-	
-    outputFilePath := filepath.Join(dirPath, nameOfFile)
-
-
-	if err != nil{
-		fmt.Println("Error creating Directory or getting path")
+	if err != nil {
+		fmt.Println("Error creating Directory or getting path:", err)
 		return err
 	}
 
-	err = SaveImage(newImg, outputFilePath, extension)
+	nameOfFile := filepath.Base(imgPath)
+	outputFilePath := filepath.Join(dirPath, nameOfFile)
 
+	err = SaveImage(newImg, outputFilePath, extension)
 	if err != nil {
 		fmt.Println("Error saving image:", err, outputFilePath)
 		return err
 	}
 
 	fmt.Printf("Image processed and saved as %s\n", outputFilePath)
-	
 	return nil
-
 }
 
 // Process images concurrently and return the first error if there was one
-func ProcessBatchImgs(files []string , theme string, processor ImageProcessor) error{
-
+func ProcessBatchImgs(files []string, theme string, processor ImageProcessor) error {
 	var wg sync.WaitGroup
 	var remaining int32 = int32(len(files))
 	errChan := make(chan error, len(files))
-	
 
-	for index, file := range files{
-		
+	for index, file := range files {
 		wg.Add(1)
 
-		go func (file string, index int)  {
+		go func(file string, index int) {
 			defer wg.Done()
 
-			err :=ProcessImg(file,processor,theme)
-
+			err := ProcessImg(file, processor, theme)
 			if err != nil {
-				 errChan <- fmt.Errorf("file %s : %w",file,err)
-				 return				
+				errChan <- fmt.Errorf("file %s: %w", file, err)
+				return
 			}
 			remainingCount := atomic.AddInt32(&remaining, -1)
-			fmt.Printf(" ::: Image %d Completed , %d Images left ::: \n",index,remainingCount)
-		}(file,index)
-
+			fmt.Printf(" ::: Image %d Completed, %d Images left ::: \n", index, remainingCount)
+		}(file, index)
 	}
 
 	wg.Wait()
 	close(errChan)
 
 	if len(errChan) > 0 {
-		return <-errChan 
+		return <-errChan
 	}
 
 	return nil

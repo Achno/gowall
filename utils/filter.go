e15f76223397b7984d7c55ec81cb0fc502a9db40
elbachir-one <bachiralfa@gmail.com>
2024-08-04 03:13:09 +0100

    Corrected minor formatting and spacing issues

--- utils/filter.go
+++ utils/filter.go
@@ -8,11 +8,10 @@ import (
 	"unicode/utf8"
 )
 
-// filters out all files other than .png .jpeg .jpg .webp in a directory
-func filterImages(entries []fs.DirEntry) ([]string, error){
-
-	if len(entries) == 0{
-		return nil,fmt.Errorf("directory is empty")
+// Filters out all files other than .png, .jpeg, .jpg, .webp in a directory
+func filterImages(entries []fs.DirEntry) ([]string, error) {
+	if len(entries) == 0 {
+		return nil, fmt.Errorf("directory is empty")
 	}
 
 	var imageFiles []string
@@ -24,17 +23,16 @@ func filterImages(entries []fs.DirEntry) ([]string, error){
 		".webp": true,
 	}
 
-	for _ , entry := range entries{
-
-		if !entry.IsDir() && supportedExtensions[strings.ToLower(filepath.Ext(entry.Name()))]{
-			imageFiles = append(imageFiles,entry.Name())
+	for _, entry := range entries {
+		if !entry.IsDir() && supportedExtensions[strings.ToLower(filepath.Ext(entry.Name()))] {
+			imageFiles = append(imageFiles, entry.Name())
 		}
 	}
 
-	return imageFiles,nil
-
+	return imageFiles, nil
 }
 
+// Discards the last character of a string
 func DiscardLastCharacter(s string) string {
 	if len(s) == 0 {
 		return s
@@ -43,6 +41,6 @@ func DiscardLastCharacter(s string) string {
 	// Decode the last rune
 	_, size := utf8.DecodeLastRuneInString(s)
 
-	// exclude the last character
+	// Exclude the last character
 	return s[:len(s)-size]
 }

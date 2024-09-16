958fb73f01a325a5c931ea30d661dc7bf432be8f
elbachir-one <bachiralfa@gmail.com>
2024-09-16 22:32:18 +0100

    Index bounds checking and URL configuration

--- internal/api/wallpaper.go
+++ internal/api/wallpaper.go
@@ -9,44 +9,40 @@ import (
 )
 
 func GetWallpaperOfTheDay() (string, error) {
-
 	url := config.WallOfTheDayUrl
 
-	response, err := http.Get(url)
-
-	if err != nil {
-		return "", err
+	if url == "" {
+		return "", fmt.Errorf("WallOfTheDayUrl is not configured")
 	}
+
+	response, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch the URL: %w", err)
+	}
+	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		return "", fmt.Errorf("request failed with status code: %d %s", response.StatusCode, http.StatusText(response.StatusCode))
 	}
 
-	defer response.Body.Close()
-
-	// Parse the html and select the top wallpaper of the day
+	// Parse HTML
 	doc, err := goquery.NewDocumentFromReader(response.Body)
-
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to parse HTML document: %w", err)
 	}
 
 	var imageUrls []string
-
 	doc.Find("img.i18n-post-media-img").Each(func(index int, selection *goquery.Selection) {
-		imgUrl, exists := selection.Attr("src")
-
-		if exists {
-			// fmt.Printf("index %d %s\n", index, imgUrl)
+		if imgUrl, exists := selection.Attr("src"); exists {
 			imageUrls = append(imageUrls, imgUrl)
 		}
 	})
 
-	// if no posts were found
-	if len(imageUrls) == 0 {
-		return "", fmt.Errorf("there wasn't a top wallpaper today :( check later")
+	// Check if any images were found
+	if len(imageUrls) < 2 {
+		return "", fmt.Errorf("no suitable wallpapers found today, try again later")
 	}
 
-	// we return the second url : example https://i.redd.it/xhupkp01d5id1.png with correct dimensions instead of the preview img
+	// Return the second URL (assuming it is of better quality)
 	return imageUrls[1], nil
 }

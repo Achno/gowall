9467ed91278a729bb0f8dd7d8e257f3653e6a0f9
Achno <129212519+Achno@users.noreply.github.com>
2024-10-18 19:45:43 +0300

    👾 Feature img to pixel art

---
+++ cmd/pixelate.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2024 Achno <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/Achno/gowall/internal/image"
+	"github.com/Achno/gowall/utils"
+	"github.com/spf13/cobra"
+)
+
+var ScaleFactor float64
+
+var pixelateCmd = &cobra.Command{
+	Use:   "pixelate [PATH]",
+	Short: "Turns an image to pixel art depending on the scale flag",
+	Long: `It can convert an image to pixel art (blocky appearance). The scale flag [1-25] controls how much the image will get pixelated. 
+		   The lower the number the more pixel effect is prevalent. 
+		   In really large images with huge resolution you may need to set the scale really low [3-8] `,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		switch {
+
+		case len(args) > 0:
+			fmt.Println("Pixelating image...")
+			processor := &image.PixelateProcessor{
+				Scale: ScaleFactor,
+			}
+			expandFile := utils.ExpandHomeDirectory(args)
+
+			path, err := image.ProcessImg(expandFile[0], processor, shared.Theme)
+			utils.HandleError(err, "Error Processing Image")
+
+			err = image.OpenImage(path)
+			utils.HandleError(err, "Error opening image")
+
+		default:
+			fmt.Println("Error: requires at least 1 arg(s), only received 0")
+			_ = cmd.Usage()
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pixelateCmd)
+	pixelateCmd.Flags().Float64VarP(&ScaleFactor, "scale", "s", 15, "Usage: --scale [1-25] (The lower the number == more pixelation)")
+
+}
---
+++ internal/image/pixelate.go
@@ -0,0 +1,73 @@
+package image
+
+import (
+	"fmt"
+	"image"
+	"math"
+)
+
+type PixelateProcessor struct {
+	Scale float64
+}
+
+func (p *PixelateProcessor) Process(img image.Image, theme string) (image.Image, error) {
+
+	// check if scale is valid
+	if p.Scale < 1 || p.Scale > 25 {
+		return nil, fmt.Errorf("scale must be between 1 and 25")
+	}
+
+	bounds := img.Bounds()
+	originalWidth := bounds.Dx()
+	originalHeight := bounds.Dy()
+
+	scaleFactor := float64(p.Scale) * 0.01
+
+	downscaled := downscale(img, scaleFactor)
+
+	// Upscale back to the original dimensions
+	upscaled := upscale(downscaled, originalWidth, originalHeight)
+
+	return upscaled, nil
+
+}
+
+func downscale(img image.Image, scale float64) image.Image {
+
+	bounds := img.Bounds()
+	width := int(math.Round(float64(bounds.Dx()) * scale))
+	height := int(math.Round(float64(bounds.Dy()) * scale))
+
+	newImage := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// pick the nearest pixel for the new scaled image
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			origX := int(float64(x) / scale)
+			origY := int(float64(y) / scale)
+			newImage.Set(x, y, img.At(origX, origY))
+		}
+	}
+
+	return newImage
+}
+
+func upscale(img image.Image, originalWidth, originalHeight int) image.Image {
+
+	newImage := image.NewRGBA(image.Rect(0, 0, originalWidth, originalHeight))
+
+	bounds := img.Bounds()
+	width := bounds.Dx()
+	height := bounds.Dy()
+
+	for y := 0; y < originalHeight; y++ {
+		for x := 0; x < originalWidth; x++ {
+			// Get the pixel from the downscaled image
+			origX := x * width / originalWidth
+			origY := y * height / originalHeight
+			newImage.Set(x, y, img.At(origX, origY))
+		}
+	}
+
+	return newImage
+}

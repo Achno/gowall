c94985f97579dd6e84c6eb0852844eac630d5472
Yusuf Yaman <153529729+nxjosephofficial@users.noreply.github.com>
2024-11-17 12:02:06 +0300

    Add FreeBSD platform support (#15)

--- utils/url.go
+++ utils/url.go
@@ -19,6 +19,8 @@ func OpenURL(url string) error {
 		cmd = exec.Command("open", url)
 	case "linux":
 		cmd = exec.Command("xdg-open", url)
+	case "freebsd":
+		cmd = exec.Command("xdg-open", url)
 
 	default:
 		return fmt.Errorf("unsupported platform")

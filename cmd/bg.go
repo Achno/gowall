abadde7ca0442055763fec43c3eaa707c2c975bf
Diego Parra <diegomiguelp1996@gmail.com>
2025-03-11 00:24:03 +0100

    refactor: Make bg cmd use the new refactored ProcessImgs function
    - Determine the imageOps from the getgo before running the command.
    
    Disclaimer this is a wip

--- cmd/bg.go
+++ cmd/bg.go
@@ -4,9 +4,9 @@ Copyright © 2024 Achno <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/Achno/gowall/internal/image"
+	imageio "github.com/Achno/gowall/internal/image_io"
+	"github.com/Achno/gowall/internal/logger"
 	"github.com/Achno/gowall/utils"
 	"github.com/spf13/cobra"
 )
@@ -20,34 +20,39 @@ var (
 
 // bgCmd represents the bg command
 var bgCmd = &cobra.Command{
-	Use:   "bg [PATH]",
+	Use:   "bg [Input]",
 	Short: "Removes the background of the image",
-	Long:  `Removes the background of an image. You can modify the options to achieve better results `,
-	Run: func(cmd *cobra.Command, args []string) {
-		switch {
-
-		case len(args) > 0:
-			fmt.Println("Removing background...")
-			processor := &image.BackgroundProcessor{}
-			processor.SetOptions(
-				image.WithConvergence(convergence),
-				image.WithMaxIter(maxIter),
-				image.WithNumRoutines(numRoutines),
-				image.WithSampleRate(sampleRate),
-			)
-
-			expandFile := utils.ExpandHomeDirectory(args)
-
-			path, _, err := image.ProcessImg(expandFile[0], processor, shared.Theme)
-			utils.HandleError(err, "Error Processing Image")
-
-			err = image.OpenImageInViewer(path)
-			utils.HandleError(err, "Error opening image")
-
-		default:
-			fmt.Println("Error: requires at least 1 arg(s), only received 0")
-			_ = cmd.Usage()
+	Long:  `Removes the background of an image. You can modify the options to achieve better results`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 || len(shared.InputDir) > 0 || len(shared.InputFiles) > 0 {
+			return
 		}
+		logger.Error("Error: no input was given, use commands args, or --dir or --batch flags")
+		_ = cmd.Usage()
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		imageOps := imageio.DetermineImageOperations(shared, args)
+		logger.Print("Removing background...")
+		processor := &image.BackgroundProcessor{}
+		processor.SetOptions(
+			image.WithConvergence(convergence),
+			image.WithMaxIter(maxIter),
+			image.WithNumRoutines(numRoutines),
+			image.WithSampleRate(sampleRate),
+		)
+		paths, err := image.ProcessImgs(processor, imageOps)
+		// Only crash when we couldn't proccess any images
+		if len(paths) == 0 {
+			utils.HandleError(err, "Error Processing Images")
+		}
+		// Otherwise print an error message for the unprocessed images
+		if err != nil {
+			logger.Errorf("the following images had errors while proccesing", err)
+		}
+		// We should only open images when we are dealing with one image and without
+		// stdout as ouput
+		err = image.OpenImageInViewer(paths[0])
+		logger.Errorf("error opening image", err)
 	},
 }
 
@@ -57,4 +62,5 @@ func init() {
 	bgCmd.Flags().IntVarP(&numRoutines, "routines", "r", 4, "")
 	bgCmd.Flags().Float64VarP(&convergence, "conv", "c", 0.001, "")
 	bgCmd.Flags().Float64VarP(&sampleRate, "sRate", "s", 0.5, "")
+	addGlobalFlags(bgCmd)
 }

f3fcfa50ead53fa10f94d5a3d3380908c2730833
Diego Parra <diegomiguelp1996@gmail.com>
2025-03-12 20:50:31 +0100

    refactor: Invert command now uses new imageio abstraction

--- cmd/invert.go
+++ cmd/invert.go
@@ -4,10 +4,9 @@ Copyright © 2024 Achnologia <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/Achno/gowall/internal/image"
+	imageio "github.com/Achno/gowall/internal/image_io"
+	"github.com/Achno/gowall/internal/logger"
 	"github.com/Achno/gowall/utils"
 	"github.com/spf13/cobra"
 )
@@ -17,45 +16,38 @@ var invertCmd = &cobra.Command{
 	Use:   "invert [image path]",
 	Short: "Inverts the color's of an image",
 	Long:  `Inverts the color's of an image , then you can convert the inverted image to your favourite color scheme`,
-	Run: func(cmd *cobra.Command, args []string) {
-		switch {
-
-		case len(shared.BatchFiles) > 0:
-			fmt.Println("Processing batch files...")
-			processor := &image.Inverter{}
-			expandedFiles := utils.ExpandHomeDirectory(shared.BatchFiles)
-			err := image.ProcessBatchImgs(expandedFiles, shared.Theme, processor)
-
-			utils.HandleError(err)
-
-		case len(args) > 0 && strings.HasSuffix(args[0], "#"):
-			fmt.Println("Processing directory...")
-			processor := &image.Inverter{}
-			path := utils.DiscardLastCharacter(args[0])
-			files, err := utils.ExpandDirectory(path)
-
-			utils.HandleError(err, "Error ExpandingHashTag")
-
-			err = image.ProcessBatchImgs(files, shared.Theme, processor)
-			utils.HandleError(err)
-
-		case len(args) > 0:
-			fmt.Println("Processing single image...")
-			processor := &image.Inverter{}
-			expandFile := utils.ExpandHomeDirectory(args)
-			path, _, err := image.ProcessImg(expandFile[0], processor, shared.Theme)
-
-			utils.HandleError(err)
-			err = image.OpenImageInViewer(path)
-			utils.HandleError(err)
-
-		default:
-			fmt.Println("Error: requires at least 1 arg(s), only received 0")
-			_ = cmd.Usage()
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		err := validateInput(shared, args)
+		if err != nil {
+			cmd.Usage()
+			return err
 		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if isInputBatch(shared) {
+			logger.Print("Processing batch files...")
+		} else {
+			logger.Print("Processing single image...")
+		}
+
+		processor := &image.Inverter{}
+
+		imageOps := imageio.DetermineImageOperations(shared, args)
+
+		processedImages, err := image.ProcessImgs(processor, imageOps, "")
+
+		if len(processedImages) == 0 {
+			utils.HandleError(err, "Error Processing Images")
+		}
+		if err != nil {
+			logger.Error(err, "The following images had errors while processing")
+		}
+		openImageInViewer(shared, args, processedImages[0])
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(invertCmd)
+	addGlobalFlags(invertCmd)
 }

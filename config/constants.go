80248d3434769c8fd8cb12e888357e937d7c01be
Achno <129212519+Achno@users.noreply.github.com>
2025-04-18 11:03:12 +0300

    fix OutputFolder for XDG_PICTURES_DIR as well

--- config/constants.go
+++ config/constants.go
@@ -22,6 +22,5 @@ func defaultConfig() Options {
 		Themes:                ThemesDefault,
 		InlineImagePreview:    InlineImagePreviewDefault,
 		ImagePreviewBackend:   ImagePreviewBackend,
-		OutputFolder:          OutputFolder,
 	}
 }

43b9cf58a3583303be2b27810cfdc0ed053edf29
Achno <129212519+Achno@users.noreply.github.com>
2025-05-12 23:59:20 +0300

    feat: added openrouter provider, brainstorming for tesseract

--- cmd/ocr.go
+++ cmd/ocr.go
@@ -54,9 +54,16 @@ to quickly create a Cobra application.`,
 		// 	// VisionLLMPrompt: "turn code to text",
 		// })
 		//? Mistral
+		// n, err := providers.NewOCRProvider(providers.Config{
+		// 	VisionLLMProvider: "mistral",
+		// 	VisionLLMModel:    "mistral-ocr-latest",
+		// 	VisionLLMPrompt:   "Extract all visible text from this image in english,Do not summarize, paraphrase, or infer missing text,Retain all spacing, punctuation, and formatting exactly as in the image,Include all text, even if it seems irrelevant or repeated.",
+		// 	// VisionLLMPrompt: "turn code to text",
+		// })
+		//? Openrouter
 		n, err := providers.NewOCRProvider(providers.Config{
-			VisionLLMProvider: "mistral",
-			VisionLLMModel:    "mistral-ocr-latest",
+			VisionLLMProvider: "openrouter",
+			VisionLLMModel:    "qwen/qwen2.5-vl-72b-instruct:free",
 			VisionLLMPrompt:   "Extract all visible text from this image in english,Do not summarize, paraphrase, or infer missing text,Retain all spacing, punctuation, and formatting exactly as in the image,Include all text, even if it seems irrelevant or repeated.",
 			// VisionLLMPrompt: "turn code to text",
 		})

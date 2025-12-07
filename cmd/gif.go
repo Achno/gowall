bfb9fec6ac817a5b3804e8127f15998db659cfa1
Achno <129212519+Achno@users.noreply.github.com>
2025-12-07 15:29:04 +0200

    refactor: gif refactored to new cmd validation

--- cmd/gif.go
+++ cmd/gif.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Achno/gowall/config"
 	"github.com/Achno/gowall/internal/image"
 	imageio "github.com/Achno/gowall/internal/image_io"
 	"github.com/Achno/gowall/internal/logger"
@@ -13,56 +14,81 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	delay  int
-	loop   int
-	resize int
-	gifCmd = &cobra.Command{
+func BuildGifCmd() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:   "gif [--batch,--dir] [PATH(S)]",
 		Short: "Create a gif Animation out of Images",
 		Long:  `Create a gif Animation out of Images specifying the delay between frames, if the gif loops forever and other options`,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			err := validateInput(shared, args)
-			if err != nil {
-				logger.Print("Error: requires at least 1 option `-b` where you specify the filePaths, or `--dir`")
-				logger.Print("Use: gowall gif -b <file,file>")
-				logger.Print("Use: gowall gif -d </path/to/dir>")
-				return err
-			}
-			if len(args) > 0 {
-				return fmt.Errorf("use --batch or --dir with gif")
-			}
-			return nil
+			return ValidateParseGifCmd(cmd, shared, args)
 		},
-		Run: func(cmd *cobra.Command, args []string) {
-			logger.Print("Creating Gif...")
-
-			options := []image.GifOption{}
-			if cmd.Flags().Changed("delay") {
-				options = append(options, image.WithDelay(delay))
-			}
-			if cmd.Flags().Changed("loop") {
-				options = append(options, image.WithLoop(loop))
-			}
-			if cmd.Flags().Changed("output") {
-				options = append(options, image.WithOutputName(shared.OutputDestination))
-			}
-			if cmd.Flags().Changed("resize") {
-				options = append(options, image.WithMode(resize))
-			}
-			imageOps, err := imageio.DetermineImageOperations(shared, args, cmd)
-			utils.HandleError(err)
-
-			err = image.CreateGif(imageOps, options...)
-			utils.HandleError(err)
-		},
+		Run: RunGifCmd,
 	}
-)
+
+	flags := cmd.Flags()
+	var (
+		delay  int
+		loop   int
+		resize int
+	)
+
+	flags.IntVarP(&delay, "delay", "d", 200, "Frame delay (ms)")
+	flags.IntVarP(&resize, "resize", "r", image.Resize, "Automatically resizes all images to the same dimensions")
+	flags.IntVarP(&loop, "loop", "l", 0, "Loop=0 (loops forever), Loop=-1 shows frames only 1 time, Loop=n (shows frames n+1)")
+
+	addGlobalFlags(cmd)
+
+	return cmd
+}
+
+func RunGifCmd(cmd *cobra.Command, args []string) {
+	logger.Print("Creating Gif...")
+
+	imageOps, err := imageio.DetermineImageOperations(shared, args, cmd)
+	utils.HandleError(err, "Error")
+
+	delay, err := cmd.Flags().GetInt("delay")
+	utils.HandleError(err, "Error")
+	loop, err := cmd.Flags().GetInt("loop")
+	utils.HandleError(err, "Error")
+	resize, err := cmd.Flags().GetInt("resize")
+	utils.HandleError(err, "Error")
+
+	options := []image.GifOption{}
+	if cmd.Flags().Changed("delay") {
+		options = append(options, image.WithDelay(delay))
+	}
+	if cmd.Flags().Changed("loop") {
+		options = append(options, image.WithLoop(loop))
+	}
+	if cmd.Flags().Changed("output") {
+		options = append(options, image.WithOutputName(shared.OutputDestination))
+	}
+	if cmd.Flags().Changed("resize") {
+		options = append(options, image.WithMode(resize))
+	}
+
+	err = image.CreateGif(imageOps, options...)
+	utils.HandleError(err, "Error")
+}
+
+func ValidateParseGifCmd(cmd *cobra.Command, flags config.GlobalSubCommandFlags, args []string) error {
+	if err := validateInput(flags, args); err != nil {
+		return err
+	}
+
+	if len(args) > 0 {
+		return fmt.Errorf("use --batch or --dir with gif with multiple images")
+	}
+
+	delay, _ := cmd.Flags().GetInt("delay")
+	if delay < 0 {
+		return fmt.Errorf("delay must be non-negative, got: %d", delay)
+	}
+
+	return nil
+}
 
 func init() {
-	rootCmd.AddCommand(gifCmd)
-	gifCmd.Flags().IntVarP(&delay, "delay", "d", 200, "Frame delay (ms)")
-	gifCmd.Flags().IntVarP(&resize, "resize", "r", image.Resize, "Automatically resizes all images to the same dimensions")
-	gifCmd.Flags().IntVarP(&loop, "loop", "l", 0, "Loop=0 (loops forever), Loop=-1 shows frames only 1 time, Loop=n (shows frames n+1)")
-	addGlobalFlags(gifCmd)
+	rootCmd.AddCommand(BuildGifCmd())
 }
